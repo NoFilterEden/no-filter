cac1ecea2c5c601c6a6561682cb248c4f59c53a0
Saleh Rabbi <33930388+salehrabbi@users.noreply.github.com>
2019-10-13 18:38:39 +0600

    all done

---
+++ resources/assets/script/common/custom.js
@@ -0,0 +1,44 @@
+$(".carosual").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 10,
+  slidesToScroll: 1,
+  prevArrow: '<i class="far fa-angle-left left"></i>',
+  nextArrow: '<i class="far fa-angle-right right"></i>',
+  autoplay: false,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
+});
+
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
