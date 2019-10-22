61cfcb2f266ad05c852623a0c8725f81292b4d52
Saleh Rabbi <33930388+salehrabbi@users.noreply.github.com>
2019-10-22 18:25:42 +0600

    overlay close

--- resources/assets/script/common/custom.js
+++ resources/assets/script/common/custom.js
@@ -48,6 +48,7 @@ $('[data-toggle="slide-collapse"]').on("click", function() {
   $navMenuCont = $($(this).data("target"));
   $navMenuCont.animate({ width: "toggle" }, 350);
   $(".main-wraper").toggleClass("active");
+  $(".overlay-full").toggleClass("active");
 });
 
 $(".search-tigger i").click(function() {
